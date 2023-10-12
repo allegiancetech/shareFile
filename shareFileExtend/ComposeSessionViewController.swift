1f1006424dcf29b3f6c27a5dff7eb02c9d8d3a69
allegiancetech <bill@allegiance-it.com>
2023-10-12 11:55:52 -0400

    Updates

--- shareFileExtend/ComposeSessionViewController.swift
+++ shareFileExtend/ComposeSessionViewController.swift
@@ -70,7 +70,7 @@ struct MountingPoints : Codable {
 
 func uploadFile(filePath: URL) -> Void {
     
-    let connect = URL(string: "https://files01.allegiance-it.com/app?operation=ws&u=support@allegiance-it.com&p=MakeItS0&s=3")!
+    let connect = URL(string: "https://files01.allegiance-it.com/app?operation=ws&u= &p=")!
     var login = URLRequest(url: connect)
     
     let task = URLSession.shared.dataTask(with: login) { data, response, error in
@@ -90,84 +90,6 @@ func uploadFile(filePath: URL) -> Void {
         if string.contains("<response code=\"3\">") {
             print("success")
         }
-        
-        /*
-        if let mimeType = httpResponse.mimeType, mimeType == "text/html",
-            let data = data,
-            let string = String(data: data, encoding: .utf8) {
-                print(string, "\n", data)
-        }
-        */
-        
-        /*
-        if let mimeType = httpResponse.mimeType, mimeType == "application/json",
-            let data = data,
-            let dataString = String(data: data, encoding: .utf8) {
-            print ("got data: \(dataString)")
-            
-            let mounts = try! JSONDecoder().decode(MountingPoints.self, from: data)
-            print (mounts.content)
-        }
-         */
     }
     task.resume()
-    
-    /*
-    do {
-        let uploadData = try Data(contentsOf: filePath)
-        
-        let url = URL(string: "https://files01.allegiance-it.com/app?operation=ws&s=11&fn=Folder+1$test.txt")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let task = URLSession.shared.uploadTask(with: request, from: uploadData) { data, response, error in
-            if let error = error {
-                print ("error: \(error)")
-                return
-            }
-            guard let response = response as? HTTPURLResponse,
-                (200...299).contains(response.statusCode) else {
-                print ("server error")
-                return
-            }
-            if let mimeType = response.mimeType,
-                mimeType == "application/json",
-                let data = data,
-                let dataString = String(data: data, encoding: .utf8) {
-                print ("got data: \(dataString)")
-            }
-        }
-        task.resume()
-    } catch {
-        return
-    }
-     */
-    
-    /*
-    let url = URL(string: "https://swapi.dev/api/people/1")!
-    var link = ""
-    let task = URLSession.shared.dataTask(with: url) { data, response, error in
-        if let error = error {
-            return
-        }
-        guard let httpResponse = response as? HTTPURLResponse,
-            (200...299).contains(httpResponse.statusCode) else {
-            return
-        }
-        if let mimeType = httpResponse.mimeType, mimeType == "text/html",
-            let data = data,
-            let string = String(data: data, encoding: .utf8) {
-                link = string
-            }
-    }
-    task.resume()
-    
-    return link
-     */
 }
-
-//https://files01.allegiance-it.com/app?operation=ws&u=support@allegiance-it.com&p=MakeItS0&src=ip&s=3
-
-//https://files01.allegiance-it.com/app?operation=ws&s=10&mp=Folder+1$&fp=/c://Users/admin/Desktop/test.txt&isupload=true
-
